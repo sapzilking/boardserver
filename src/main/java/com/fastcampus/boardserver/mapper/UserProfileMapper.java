d8baa64dec4958b4386cb883ff12e1104f80098e
sapzilking <sapzilking@gmail.com>
2024-06-06 11:29:33 +0900

    enh: 사용자 API 개발

---
+++ src/main/java/com/fastcampus/boardserver/mapper/UserProfileMapper.java
@@ -0,0 +1,49 @@
+package com.fastcampus.boardserver.mapper;
+
+import com.fastcampus.boardserver.dto.UserDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface UserProfileMapper {
+    public UserDTO getUserProfile(@Param("id") String id);
+
+    int insertUserProfile(@Param("id") String id, @Param("password") String password, @Param("name") String name, @Param("phone") String phone, @Param("address") String address);
+
+    int updateUserProfile(@Param("id") String id, @Param("password") String password, @Param("name") String name, @Param("phone") String phone, @Param("address") String address);
+
+    int deleteUserProfile(@Param("id") String id);
+
+    public int register(UserDTO userDTO);
+
+    public UserDTO findByIdAndPassword(@Param("id") String id,
+                                       @Param("password") String password);
+
+    public UserDTO findByUserIdAndPassword(@Param("userId") String userId,
+                                           @Param("password") String password);
+
+    int idCheck(String id);
+
+    public int updatePassword(UserDTO userDTO);
+
+    public int updateAddress(UserDTO userDTO);
+
+
+//    public UserDTO getUserProfile(@Param("userId") String id);
+//
+//    int insertUserProfile(@Param("id") String id, @Param("password") String password,
+//                          @Param("name") String name, @Param("phone") String phone,
+//                          @Param("address") String address, @Param("createTime") String createTime,
+//                          @Param("updateTime") String updateTime);
+//
+//    int deleteUserProfile(@Param("id") String id);
+//
+//    public UserDTO findByIdAndPassword(@Param("userId") String id, @Param("password") String password);
+//
+//    int idCheck(String id);
+//
+//    public int updatePassword(UserDTO user);
+//    public int updateAddress(UserDTO user);
+//
+//    int register(UserDTO userProfile);
+}
